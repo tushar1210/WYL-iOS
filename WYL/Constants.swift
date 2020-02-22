0beadea861dda9d0ef4c23edf4c1b3fe13680048
tushar.1210 <ts26192271@gmail.com>
2020-02-22 23:55:37 +0530

    .

---
+++ WYL/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  WYL
+//
+//  Created by Tushar Singh on 22/02/20.
+//  Copyright © 2020 Tushar Singh. All rights reserved.
+//
+
+import Foundation
+
+var base:String = "http://203.92.44.186/wyl.com/"
