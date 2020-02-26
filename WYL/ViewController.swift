482070308f01be76bea79522ac0fd36c3316d4f2
tushar.1210 <ts26192271@gmail.com>
2020-02-26 22:05:54 +0530

    welcome screen

---
+++ WYL/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WYL
+//
+//  Created by Tushar Singh on 26/02/20.
+//  Copyright © 2020 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var goButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        goButton.roundCorners()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WYL/extension.swift
@@ -0,0 +1,17 @@
+//
+//  extension.swift
+//  WYL
+//
+//  Created by Tushar Singh on 26/02/20.
+//  Copyright © 2020 Tushar Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton{
+    func roundCorners(){
+        self.layer.cornerRadius = self.frame.height/2
+    }
+}
+
